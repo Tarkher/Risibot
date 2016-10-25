18fb609b88f607ae7f3e90f6863484afb244a057
Risitop <aziz.foucheasnoun@gmail.com>
2016-10-25 17:13:24 +0200

    First test file

---
+++ JS/tests.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Risibot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Risitop
+// @match        http://play.pokemonshowdown.com/*
+// @grant        none
+// ==/UserScript==
+
+function getRoom() { // Returns current battle object
+    url = window.location.href;
+    while (url[0] != 'b') {
+        url = url.slice(1);
+    }
+    return app.rooms[url];
+}
+
+function waitingForMe(room) {
+	if (!room)
+		return false;
+	else if (!room.choice)
+		return false;
+	else if (!room.choice.waiting)
+		return true;
+	return false;
+}
+
+function attack(room, id) {
+	moves = document.getElementsByName("chooseMove");
+	if (id < moves.length)
+		room.chooseMove(id + 1, moves[id]);
+}
+
+function main(ctx) {
+
+    if (!ctx) {
+        ctx = { "room": getRoom() };
+    }
+
+    if (waitingForMe(ctx.room)) {
+       attack(ctx.room, parseInt(Math.random() * 4));
+    }
+
+    setTimeout( function() { main(ctx); }, 500 );
+}
+
+window.onload = main(null);
