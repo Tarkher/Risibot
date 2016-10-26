0fc38413b43c41083a4edae0a30bd4baa3a2720e
Risitop <aziz.foucheasnoun@gmail.com>
2016-10-27 01:40:37 +0200

    Update PokeyI.js

--- JS/PokeyI.js
+++ JS/PokeyI.js
@@ -4,19 +4,19 @@ PokeyI = function(bot) {
 };
 
 BattleRoles = {
-	
-	'sweeperAtk': 0,
-	'sweeperSpa': 1,
-	
-	'wallDef': 2,
-	'wallSpd': 3,
-	
-	'pivotOff': 4,
-	'pivotDef': 5,
-	
-	'wallbreakOff': 6,
-	'wallbreakDef': 7
-}
+
+  'sweeperAtk': 0,
+  'sweeperSpa': 1,
+
+  'wallDef': 2,
+  'wallSpd': 3,
+
+  'pivotOff': 4,
+  'pivotDef': 5,
+
+  'wallbreakOff': 6,
+  'wallbreakDef': 7
+};
 
 PokeyI.prototype.evalDamagingMove = function(move) {
 
@@ -153,7 +153,7 @@ PokeyI.prototype.hasType = function(pokemon, type) {
 };
 
 PokeyI.prototype.getStat = function(pokemon, stat) {
-	return pokemon.baseStats[stat];
+  return pokemon.baseStats[stat];
 };
 
 PokeyI.prototype.getBulk = function(pokemon) {
@@ -189,99 +189,99 @@ PokeyI.prototype.getMultiplicator = function(pokemon, stat) {
 };
 
 PokeyI.prototype.evalSwitch = function(pokemon) {
-	var coef = 100.0;
-	var weaknesses = this.getWeaknesses(pokemon);
-	for (var i = 0; i < this.bot.ennemy[0].types; i++)
-		coef *= (1.0 / weaknesses[this.bot.ennemy[0][i]);
-	
-	
+  var coef = 100.0;
+  var weaknesses = this.getWeaknesses(pokemon);
+  for (var i = 0; i < this.bot.ennemy[0].types; i++)
+    coef *= (1.0 / weaknesses[this.bot.ennemy[0][i]]);
+
+
 };
 
 PokeyI.prototype.getExpectedRole = function(pokemon) {
-	// val in BattleRoles
-	potentialRoles = [];
-	
-	// Simple cartesian norma
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'spe'))); //swp a
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'spa'), this.getStat(pokemon, 'spe'))); // swp s
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'hp'), this.getStat(pokemon, 'def'))); // wall def
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'hp'), this.getStat(pokemon, 'spd'))); // wall off
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'spe'))); // piv off
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'def'), this.getStat(pokemon, 'spd'))); // piv def
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'atk'))); // wb off
-	potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'def'))); // wb def
-	
-	// We apply more coefs
-	potentialRoles[BattleRoles.wallDef] *= this.getPotentialWall(pokemon, 'def');
-	potentialRoles[BattleRoles.wallSpd] *= this.getPotentialWall(pokemon, 'spd');
-	
-	return potentialRoles;
-	
+  // val in BattleRoles
+  potentialRoles = [];
+
+  // Simple cartesian norma
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'spe'))); //swp a
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'spa'), this.getStat(pokemon, 'spe'))); // swp s
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'hp'), this.getStat(pokemon, 'def'))); // wall def
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'hp'), this.getStat(pokemon, 'spd'))); // wall off
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'spe'))); // piv off
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'def'), this.getStat(pokemon, 'spd'))); // piv def
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'atk'))); // wb off
+  potentialRoles.push(this.distance(this.getStat(pokemon, 'atk'), this.getStat(pokemon, 'def'))); // wb def
+
+  // We apply more coefs
+  potentialRoles[BattleRoles.wallDef] *= this.getPotentialWall(pokemon, 'def');
+  potentialRoles[BattleRoles.wallSpd] *= this.getPotentialWall(pokemon, 'spd');
+
+  return potentialRoles;
+
 };
 
 PokeyI.prototype.distance = function(a, b) {
-	return Math.sqrt(a*a + b*b)
-}
+  return Math.sqrt(a * a + b * b);
+};
 
 PokeyI.prototype.getPotentialWall = function(pokemon, type) { // Can pokemon be a good wall ?
-	coef = Math.pow(2, parseFloat(this.getStat(pokemon, type) - 80) / 30);
-	if (this.canHeal(pokemon)) // Increases greatly the staying power
-		coef *= 1.5;
-	else
-		coef *= 0.67;
-	if (this.canCure(pokemon)) // Not as important, but cool.
-		coef *= 1.2;
-	return coef * this.passiveHeal(pokemon); // Leech seed / ingrain ... / POISON HEAL !!
+  coef = Math.pow(2, parseFloat(this.getStat(pokemon, type) - 80) / 30);
+  if (this.canHeal(pokemon)) // Increases greatly the staying power
+    coef *= 1.5;
+  else
+    coef *= 0.67;
+  if (this.canCure(pokemon)) // Not as important, but cool.
+    coef *= 1.2;
+  return coef * this.passiveHeal(pokemon); // Leech seed / ingrain ... / POISON HEAL !!
 };
 
 PokeyI.prototype.canHeal = function(pokemon) { // If pokemon can learn viable heal moves
-	name = pokemon.species.toLowerCase();
-	for (move in BattleLearnsets[name].learnset) {
-		switch (move) {
-			case 'roost':
-			case 'recover':
-			case 'slackoff':	
-			case 'softboiled':
-			case 'milkdrink':
-			case 'synthesis':
-			case 'morningsun':
-			case 'moonlight':
-			case 'wish':
-			case 'healorder':
-				return true;
-		}
-	}
-	return false;
+  name = pokemon.species.toLowerCase();
+  for (move in BattleLearnsets[name].learnset) {
+    switch (move) {
+      case 'roost':
+      case 'recover':
+      case 'slackoff':
+      case 'softboiled':
+      case 'milkdrink':
+      case 'synthesis':
+      case 'morningsun':
+      case 'moonlight':
+      case 'wish':
+      case 'healorder':
+        return true;
+    }
+  }
+  return false;
 };
 
 PokeyI.prototype.canCure = function(pokemon) { // If pokemon can learn viable heal moves
-	name = pokemon.species.toLowerCase();
-	for (move in BattleLearnsets[name].learnset) {
-		switch (move) {
-			case 'healbell':
-			case 'aromatherapy':
-				return true;
-		}
-	}
-	return false;
+  name = pokemon.species.toLowerCase();
+  for (move in BattleLearnsets[name].learnset) {
+    switch (move) {
+      case 'healbell':
+      case 'aromatherapy':
+        return true;
+    }
+  }
+  return false;
 };
 
 PokeyI.prototype.passiveHeal = function(pokemon) { // If pokemon can learn viable heal moves
-	var coef = 1.0;
-	name = pokemon.species.toLowerCase();
-	for (move in BattleLearnsets[name].learnset) {
-		switch (move) {
-			case 'aquaring':
-			case 'leechseed':
-			case 'ingrain':
-				coef *= 1.1;
-		}
-	}
-	for (a in BattlePokedex[name].abilities) {
-		switch (BattlePokedex[name].abilities[a]) {
-			case 'Poison Heal':
-				coef *= 1.5;
-		}
-	}
-	return coef;
+  var coef = 1.0;
+  name = pokemon.species.toLowerCase();
+  for (move in BattleLearnsets[name].learnset) {
+    switch (move) {
+      case 'aquaring':
+      case 'leechseed':
+      case 'ingrain':
+        coef *= 1.1;
+    }
+  }
+  for (a in BattlePokedex[name].abilities) {
+    switch (BattlePokedex[name].abilities[a]) {
+      case 'Poison Heal':
+        coef *= 1.5;
+    }
+  }
+  return coef;
 };
