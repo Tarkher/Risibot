4c1b89517cfc9028c04924cefcbcd5562d684721
Risitop <aziz.foucheasnoun@gmail.com>
2016-10-29 01:14:27 +0200

    Extension

---
+++ Risibot.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Risibot
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  try to take over the world!
+// @author       Risitop
+// @match        http://play.pokemonshowdown.com/*
+// @require      https://raw.githubusercontent.com/Zarel/Pokemon-Showdown-Client/master/data/learnsets-g6.js
+// @require      https://pokemonshowdown.com/damagecalc/js/lodash.min.js
+// @require      https://pokemonshowdown.com/damagecalc/select2/select2.min.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/pokedex.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/setdex_xy.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/setdex_bw.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/setdex_dpp.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/setdex_rse.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/setdex_gsc.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/setdex_rby.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/stat_data.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/type_data.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/nature_data.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/ability_data.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/item_data.js
+// @require      https://pokemonshowdown.com/damagecalc/js/data/move_data.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/ap_calc.js
+// @require      https://pokemonshowdown.com/damagecalc/js/damage.js
+// @require      https://pokemonshowdown.com/damagecalc/js/damage_dpp.js
+// @require      https://pokemonshowdown.com/damagecalc/js/damage_rse.js
+// @require      https://pokemonshowdown.com/damagecalc/js/damage_gsc.js
+// @require      https://pokemonshowdown.com/damagecalc/js/damage_rby.js
+// @require      https://pokemonshowdown.com/damagecalc/js/ko_chance.js
+// @require      https://pokemonshowdown.com/damagecalc/js/moveset_import.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/Tools.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/PokeyI.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/Risibot.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/FullPokemon.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/ap_calc_fix.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/Risibot.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/moveEvaluations.js
+// @require      https://raw.githubusercontent.com/Risitop/Risibot/master/JS/RisibotWatcher.js
+// @grant        none
+// ==/UserScript==
+///////////////////////////////////////////////////////////////////////////////
